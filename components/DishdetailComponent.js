a17892f88b164fedec0ab97b133024f8f631f7d8
saitsuer <saitsuer@gmail.com>
2021-06-07 03:17:24 +0300

    nav-part-1

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -3,15 +3,19 @@ import { Text, View } from 'react-native';
 import { Card } from 'react-native-elements';
 import { DISHES } from '../shared/dishes';
 
+//<Card.Title>{dish.name}</Card.Title>
+//<Card.image source={require('./images/uthappizza.png')}></Card.image>
+
 function RenderDish(props) {
 
     const dish = props.dish;
     
         if (dish != null) {
             return(
-                <Card
-                featuredTitle={dish.name}
-                image={require('./images/uthappizza.png')}>
+                <Card>
+                    <Card.Title>{dish.name}</Card.Title>
+                    <Card.Divider/>
+                    <Card.Image source={require('./images/uthappizza.png')}></Card.Image>
                     <Text style={{margin: 10}}>
                         {dish.description}
                     </Text>
